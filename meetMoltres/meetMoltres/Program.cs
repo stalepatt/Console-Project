05e9a5a37174805734ca0a6feafa9220366008e2
Olive You <cheeeedy@gmail.com>
2023-01-21 18:21:54 +0900

    콘솔 초기 세팅 및 플레이어 이동 구현

---
+++ meetMoltres/meetMoltres/Program.cs
@@ -0,0 +1,50 @@
+﻿class Sokoban
+{
+    static void Main()
+    {
+        // Console Initial Settings
+        Console.ResetColor();
+        Console.CursorVisible = false;
+        Console.Title = "LegendaryMoltres";
+        Console.BackgroundColor = ConsoleColor.Gray;
+        Console.Clear();
+
+
+        
+        int playerX = 5;
+        int playerY = 5;
+        
+        while (true)
+        {
+            //---------render---------
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(playerX, playerY);
+            Console.Write("P");
+
+            //---------입력---------
+            ConsoleKey key = Console.ReadKey().Key;
+            
+            //---------처리---------
+            if (key == ConsoleKey.LeftArrow)
+            {
+                playerX = Math.Clamp(playerX - 1, 0, 15);
+            }
+            if (key == ConsoleKey.RightArrow)
+            {
+                playerX = Math.Clamp(playerX + 1, 0, 15);
+            }
+            if (key == ConsoleKey.UpArrow)
+            {
+                playerY = Math.Clamp(playerY - 1, 0, 15);
+            }
+            if (key == ConsoleKey.DownArrow)
+            {
+                playerY = Math.Clamp(playerY + 1, 0, 15);
+            }
+        }
+
+
+
+    }
+}
